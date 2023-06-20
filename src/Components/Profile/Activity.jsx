437acae035759234c6dfcedc00230c2218a8036e
ahmedessamrizk <ahmed.essam7722@gmail.com>
2023-06-20 22:19:14 +0300

    update

---
+++ src/Components/Profile/Activity.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function Activity() {
+  return (
+    <>
+      <div className="row gutters-sm">
+        <div className="col">
+          <div className="card">
+            <div className="card-body">
+              <div className="row">
+                <p className="ps-3">
+                  You have added <span className="text-info">game</span>
+                </p>
+              </div>
+              <hr className="my-0" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -1,18 +1,18 @@
 import React, { useEffect, useState } from "react";
 import "./Profile.css";
-import { Link, useParams } from "react-router-dom";
+import { Link, Outlet, useNavigate, useParams } from "react-router-dom";
 import axios from "axios";
 import { baseURL, BEARERKEY } from "./../../index.js";
+import Info from "./Info.jsx";
 
 export default function Profile() {
   const [profile, setProfile] = useState(null);
-  const [wishList, setWishList] = useState([])
   const getProfile = async () => {
     const config = {
-        headers: {
-          authorization: BEARERKEY + localStorage.getItem("token"),
-        },
-      };
+      headers: {
+        authorization: BEARERKEY + localStorage.getItem("token"),
+      },
+    };
     const result = await axios
       .get(`${baseURL}/user/profile`, config)
       .catch(function (error) {
@@ -20,160 +20,77 @@ export default function Profile() {
           console.log(error.response);
         }
       });
-    if(result?.data?.message  == "done"){
-        setProfile(result.data.user)
+    if (result?.data?.message == "done") {
+      setProfile(result.data.user);
     }
   };
-  const getWishList = async () => {
-    const config = {
-        headers: {
-          authorization: BEARERKEY + localStorage.getItem("token"),
-        },
-      };
-    const result = await axios
-      .get(`${baseURL}/user/wishlist`, config)
-      .catch(function (error) {
-        if (error.response) {
-          console.log(error.response);
-        }
-      });
-    if(result?.data?.message  == "done"){
-        setWishList(result.data.wishList)
-    }
-  };
+  function modifyButtons(e) {
+    // ${e.target}
+  }
   useEffect(() => {
     getProfile();
-    getWishList();
   }, []);
 
-
-  return <>
-      {
-        profile?
+  return (
+    <>
+      {profile ? (
         <div className="container profile">
-        <header className="d-flex justify-content-between align-items-center mb-2">
-          <Link to="/home" className="back-store h4 fw-bolder">
-            <i className="fa-solid fa-arrow-left me-2"></i>
-            <strong>Store</strong>
-          </Link>
-        </header>
-        <div className="main-body">
-          <nav aria-label="breadcrumb" className="main-breadcrumb">
-            <ol className="breadcrumb"></ol>
-          </nav>
-
-          <div className="row gutters-sm">
-            <div className="col-md-4 mb-3">
-              <div className="card">
-                <div className="card-body">
-                  <div className="d-flex flex-column align-items-center ">
-                    <img
-                      src={profile.profilePic?.secure_url }
-                      alt="Profile Picture"
-                      className="rounded-circle"
-                      width="150"
-                    />
-                    <div className="mt-3">
-                      <h4>{profile.firstName} {profile.lastName}</h4>
-                      <h6 className="text-muted text-center">{profile.userName}</h6>
-                      <p className="text-muted fs-6 mb-0">Followers: {profile.followings}</p>
-                      <p className="text-muted fs-6">Following: </p>
+          <header className="d-flex justify-content-between align-items-center mb-2">
+            <Link to="/home" className="back-store h4 fw-bolder">
+              <i className="fa-solid fa-arrow-left me-2"></i>
+              <strong>Store</strong>
+            </Link>
+          </header>
+          <div className="main-body">
+            <div className="row">
+              <div className="col-md-4">
+                <div className="card">
+                  <div className="card-body">
+                    <div className="d-flex flex-column align-items-center ">
+                      <img
+                        src={profile.profilePic?.secure_url}
+                        alt="Profile Picture"
+                        className="rounded-circle img-fluid"
+                        width="150"
+                      />
+                      <div className="mt-3">
+                        <h4>
+                          {profile.firstName} {profile.lastName}
+                        </h4>
+                        <h6 className="text-muted text-center">
+                          username: {profile.userName}
+                        </h6>
+                      </div>
                     </div>
                   </div>
                 </div>
               </div>
-            </div>
-            <div className="col-md-8">
-              <div className="card mb-3">
-                <h3> Personal Information </h3>
-                <div className="card-body">
-                  <div className="row">
-                    <div className="col-sm-3">
-                      <h6 className="mb-0">First Name</h6>
-                    </div>
-                    <div className="col-sm-9 text-secondary">
-                      {profile.firstName}
-                    </div>
-                  </div>
-                  <hr />
-                  <div className="row">
-                    <div className="col-sm-3">
-                      <h6 className="mb-0">Last Name</h6>
-                    </div>
-                    <div className="col-sm-9 text-secondary">{profile.lastName}</div>
-                  </div>
-                  <hr />
-                  <div className="row">
-                    <div className="col-sm-3">
-                      <h6 className="mb-0">Date of Birth</h6>
-                    </div>
-                    <div className="col-sm-9 text-secondary">{profile.DOB}</div>
-                  </div>
-                  <hr />
-                  <div className="row">
-                    <div className="col-sm-3">
-                      <h6 className="mb-0">Email</h6>
-                    </div>
-                    <div className="col-sm-9 text-secondary">{profile.email}</div>
-                  </div>
-                  <hr />
-                  <div className="row">
-                    <div className="col-sm-3">
-                      <h6 className="mb-0">Phone</h6>
-                    </div>
-                    <div className="col-sm-9 text-secondary">
-                    {profile.phone}
-                    </div>
-                  </div>
-                  <hr />
-                  <div className="row">
-                    <div className="col-sm-3">
-                      <h6 className="mb-0">Age</h6>
-                    </div>
-                    <div className="col-sm-9 text-secondary">{profile.age}</div>
-                  </div>
-                  <hr />
-                  <div className="row">
-                    <div className="col-sm-3">
-                      <h6 className="mb-0">Address</h6>
-                    </div>
-                    <div className="col-sm-9 text-secondary">
-                    {profile.address}
-                    </div>
-                  </div>
-                  <hr />
+              <div className="col-md-8">
+                <div className="card mb-3">
+                  <ul class="fs-6 d-flex justify-content-between py-3 px-4 mb-0 rounded-2">
+                    <li class="nav-item" onClick={modifyButtons}>
+                      <Link to="info">Info</Link>
+                    </li>
+                    <li class="nav-item " onClick={modifyButtons}>
+                      <Link to="activity">Activity</Link>
+                    </li>
+                    <li class="nav-item" onClick={modifyButtons}>
+                      <Link to="wishlist">Wishlist</Link>
+                    </li>
+                    <li class="nav-item" onClick={modifyButtons}>
+                      <Link to="following">Followers</Link>
+                    </li>
+                    <li class="nav-item">
+                      <Link to="games">Games</Link>
+                    </li>
+                  </ul>
                 </div>
-              </div>
-
-              <div className="row gutters-sm">
-                <h3>WishList</h3>
-                <div className="col-sm-12 mb-3">
-                  <div className="card h-100">
-                    <div className="card-body ps-0">
-                        {
-                            wishList?.map((game, idx) =>
-                                    <div key={idx} className="game-check my-2 rounded-4 text-white-50 d-flex justify-content-between align-items-center">
-                                        <div className="d-flex ">
-
-                                        <img src={game.mainPic?.secure_url} className="img-fluid rounded" width={100} height={100} alt="" srcSet="" />
-                                        <h6 className="game-name m-0 text-center p-3">{game.name}</h6>
-                                        </div>
-                                        <div className="lefted pe-3">
-                                            <span>${game.price}</span>
-                                            <button className='close-btn rounded-circle bg-transparent text-white ms-3'>X</button>
-                                        </div>
-                                    </div>
-                            )
-                        }
-                    </div>
-                  </div>
-                </div>
+                <Outlet context={[profile]} />
               </div>
             </div>
           </div>
         </div>
-      </div>
-        :
+      ) : (
         <div className="w-100 vh-100 d-flex justify-content-center align-items-center position-absolute top-0">
           <div className="sk-chase">
             <div className="sk-chase-dot"></div>
@@ -184,7 +101,7 @@ export default function Profile() {
             <div className="sk-chase-dot"></div>
           </div>
         </div>
-      }
+      )}
     </>
-
+  );
 }
