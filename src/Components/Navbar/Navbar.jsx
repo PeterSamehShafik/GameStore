3c54960f90153826927998ba5ad5d26604e898ab
ahmedessamrizk <ahmed.essam7722@gmail.com>
2023-07-01 09:40:12 +0300

    push modifications on login and navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -35,8 +35,9 @@ function Navbar({ currentUser, removeUser, cart, setSearch }) {
           >
             <span className="navbar-toggler-icon text-white"></span>
           </button>
+          
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            {!location.pathname.toLowerCase().includes("home")  || !currentUser ? (
+            {!location.pathname.toLowerCase().includes("home") ? (
               ""
             ) : (
               <div
